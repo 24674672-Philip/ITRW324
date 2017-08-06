1225373d24fbadd998adef0621b986fcf1e61d04
Vorster Naude <vorsternaude@gmail.com>
2017-08-06 12:29:43 +0200

    Project created and buttons added

---
+++ ITRW324_Git_Assignment/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ITRW324_Git_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVorster = new System.Windows.Forms.Button();
+            this.btnPhilip = new System.Windows.Forms.Button();
+            this.btnZander = new System.Windows.Forms.Button();
+            this.btnBrendan = new System.Windows.Forms.Button();
+            this.btnTheunnis = new System.Windows.Forms.Button();
+            this.btnJoy = new System.Windows.Forms.Button();
+            this.btnKeagan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVorster
+            // 
+            this.btnVorster.Location = new System.Drawing.Point(112, 27);
+            this.btnVorster.Name = "btnVorster";
+            this.btnVorster.Size = new System.Drawing.Size(173, 29);
+            this.btnVorster.TabIndex = 0;
+            this.btnVorster.Text = "Vorster";
+            this.btnVorster.UseVisualStyleBackColor = true;
+            // 
+            // btnPhilip
+            // 
+            this.btnPhilip.Location = new System.Drawing.Point(112, 69);
+            this.btnPhilip.Name = "btnPhilip";
+            this.btnPhilip.Size = new System.Drawing.Size(173, 29);
+            this.btnPhilip.TabIndex = 1;
+            this.btnPhilip.Text = "Philip";
+            this.btnPhilip.UseVisualStyleBackColor = true;
+            // 
+            // btnZander
+            // 
+            this.btnZander.Location = new System.Drawing.Point(112, 119);
+            this.btnZander.Name = "btnZander";
+            this.btnZander.Size = new System.Drawing.Size(173, 29);
+            this.btnZander.TabIndex = 2;
+            this.btnZander.Text = "Zander";
+            this.btnZander.UseVisualStyleBackColor = true;
+            // 
+            // btnBrendan
+            // 
+            this.btnBrendan.Location = new System.Drawing.Point(112, 167);
+            this.btnBrendan.Name = "btnBrendan";
+            this.btnBrendan.Size = new System.Drawing.Size(173, 29);
+            this.btnBrendan.TabIndex = 3;
+            this.btnBrendan.Text = "Theunnis";
+            this.btnBrendan.UseVisualStyleBackColor = true;
+            // 
+            // btnTheunnis
+            // 
+            this.btnTheunnis.Location = new System.Drawing.Point(112, 210);
+            this.btnTheunnis.Name = "btnTheunnis";
+            this.btnTheunnis.Size = new System.Drawing.Size(173, 29);
+            this.btnTheunnis.TabIndex = 4;
+            this.btnTheunnis.Text = "Brendan";
+            this.btnTheunnis.UseVisualStyleBackColor = true;
+            // 
+            // btnJoy
+            // 
+            this.btnJoy.Location = new System.Drawing.Point(112, 254);
+            this.btnJoy.Name = "btnJoy";
+            this.btnJoy.Size = new System.Drawing.Size(173, 29);
+            this.btnJoy.TabIndex = 5;
+            this.btnJoy.Text = "Joy";
+            this.btnJoy.UseVisualStyleBackColor = true;
+            // 
+            // btnKeagan
+            // 
+            this.btnKeagan.Location = new System.Drawing.Point(112, 302);
+            this.btnKeagan.Name = "btnKeagan";
+            this.btnKeagan.Size = new System.Drawing.Size(173, 29);
+            this.btnKeagan.TabIndex = 6;
+            this.btnKeagan.Text = "Keagan";
+            this.btnKeagan.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 393);
+            this.Controls.Add(this.btnKeagan);
+            this.Controls.Add(this.btnJoy);
+            this.Controls.Add(this.btnTheunnis);
+            this.Controls.Add(this.btnBrendan);
+            this.Controls.Add(this.btnZander);
+            this.Controls.Add(this.btnPhilip);
+            this.Controls.Add(this.btnVorster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVorster;
+        private System.Windows.Forms.Button btnPhilip;
+        private System.Windows.Forms.Button btnZander;
+        private System.Windows.Forms.Button btnBrendan;
+        private System.Windows.Forms.Button btnTheunnis;
+        private System.Windows.Forms.Button btnJoy;
+        private System.Windows.Forms.Button btnKeagan;
+    }
+}
+
