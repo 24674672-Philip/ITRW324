795812160e093f07617c084e276cd6831b6b5314
Vorster Naude <26192349@student.nwu.ac.za>
2017-08-06 14:29:27 +0200

    Almost done with standard dev

---
+++ ITRW324_Git_Assignment/Standard_Dev_Class.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITRW324_Git_Assignment
+{
+    class Standard_Dev_Class
+    {
+        
+
+        public Standard_Dev_Class(int[] data)
+        {
+            calculateStandardDeviation(data);
+        }
+
+        private double calculateStandardDeviation(int[] pData)
+        {
+            int total = 0;
+            int ave;
+            for(int i =0; i<pData.Length; i++)
+            {
+                total += pData[i];
+            }
+            ave = total / pData.Length;
+
+            for(int i=0;i<pData.Length; i++)
+            {
+
+            }
+
+
+
+
+
+
+
+
+
+
+            return 0.00;
+        }
+    }
+}
