93005d2a0df542e9f8ceb4d1ad1ca086952a41ad
Philip <24674672@student.g.nwu.ac.za>
2017-08-21 22:51:13 +0200

    Webservice is up and running
    requires error handling still

--- Web Service/webservice.js
+++ Web Service/webservice.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 var url = require('url');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "mydb"
+  host: "52.15.226.85",
+  user: "philip",
+  password: "blockchain",
+  database: "blockchainDB"
 });
 
 /* con.connect(function(err) {
@@ -21,26 +21,20 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url,true).query;
   res.writeHead(200, {'Content-Type': 'text/plain'});
   if(q.type === 'login'){
-	var sql = 'SELECT * FROM users WHERE user_name = ? AND user_password = ?';
-	con.connect(function(err) {
-	  if (err) throw err;
+	  var sql = 'SELECT * FROM users WHERE name = ? AND password = ?';
 	  con.query(sql, [q.user, q.pass], function (err, result) {
 	    if (err) throw err;
 	    console.log(result);
       res.end('Success');
 	  });
-	});
   }
   else if(q.type === 'register'){
-	var sql = 'INSERT INTO users (user_name, user_surname, user_password) VALUES ?';
+	var sql = 'INSERT INTO users (name, surname, password) VALUES ?';
   var valIn = [[q.user, q.sur, q.pass]];
-	con.connect(function(err) {
-	  if (err) throw err;
 	  con.query(sql, [valIn], function (err, result) {
 	    if (err) throw err;
 	    console.log(result);
       res.end('Registered');
 	  });
-	});
   }
 }).listen(8080);
