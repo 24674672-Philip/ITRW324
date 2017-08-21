4e64d5adfe9d290e851f7119873523a63121b71b
Vorster Naude <26192349@student.nwu.ac.za>
2017-08-21 16:53:33 +0200

    homeContainer added along with the navigation template for the rest of the project

--- Website/RIPWebsite/src/app/app.module.ts
+++ Website/RIPWebsite/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './loginComponents/banner/banner.component';
 import { InputformComponent } from './loginComponents/inputform/inputform.component';
 import { ServerstatusComponent } from './loginComponents/serverstatus/serverstatus.component';
+import { NavBarComponent } from './ripComponents/home-container/nav-bar/nav-bar.component';
+import { MusicListComponent } from './ripComponents/home-container/music-list/music-list.component';
+import { MusicItemComponent } from './ripComponents/home-container/music-list/music-item/music-item.component';
+import { HomeContainerComponent } from './ripComponents/home-container/home-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     InputformComponent,
-    ServerstatusComponent
+    ServerstatusComponent,
+    NavBarComponent,
+    MusicListComponent,
+    MusicItemComponent,
+    HomeContainerComponent
   ],
   imports: [
     BrowserModule
