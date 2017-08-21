41df0487ac67f25abbe22ad9d9c8cccda2d086e7
Philip <24674672@student.g.nwu.ac.za>
2017-08-21 10:22:00 +0200

    Added packages

--- Web Service/webservice.js
+++ Web Service/webservice.js
@@ -1,22 +1,16 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'mydb'
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "mydb"
 });
 
-connection.connect();
-
-var mydbEntry = {
-  user_name: 'Philip'
-};
-
-connection.query('insert into users set ?', mydb, function(err, result){
-  if(err){
-    console .error(err);
-    return;
-  }
-  console.console.error((result));
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM users", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
