b4d716a4d5522411890300ac16e52ec3570c93e0
Philip <24674672@student.g.nwu.ac.za>
2017-08-21 17:54:06 +0200

    Fixed the webservice

--- Web Service/webservice.js
+++ Web Service/webservice.js
@@ -9,35 +9,38 @@ var con = mysql.createConnection({
   database: "mydb"
 });
 
-con.connect(function(err) {
+/* con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM users", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
-});
+}); */
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url,true);
+  var q = url.parse(req.url,true).query;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   if(q.type === 'login'){
-	var sql = 'SELECT * FROM users WHERE user_name = ? AND user_pass = ?';
+	var sql = 'SELECT * FROM users WHERE user_name = ? AND user_password = ?';
 	con.connect(function(err) {
 	  if (err) throw err;
 	  con.query(sql, [q.user, q.pass], function (err, result) {
 	    if (err) throw err;
 	    console.log(result);
-	  });	
-	});  
+      res.end('Success');
+	  });
+	});
   }
   else if(q.type === 'register'){
-	var sql = 'INSERT INTO users (?) VALUES ?';
-	var val = ;
+	var sql = 'INSERT INTO users (user_name, user_surname, user_password) VALUES ?';
+  var valIn = [[q.user, q.sur, q.pass]];
 	con.connect(function(err) {
 	  if (err) throw err;
-	  con.query(sql, [val], function (err, result) {
+	  con.query(sql, [valIn], function (err, result) {
 	    if (err) throw err;
 	    console.log(result);
-	  });	
-	});      
-  }	  
+      res.end('Registered');
+	  });
+	});
+  }
 }).listen(8080);
