a31d00fda6d95966981960b3082f4d63552eeb19
philip <24674672@student.g.nwu.ac.za>
2017-08-21 16:03:59 +0200

    added register and login

--- Web Service/webservice.js
+++ Web Service/webservice.js
@@ -1,4 +1,6 @@
+var http = require('http');
 var mysql = require('mysql');
+var url = require('url');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -14,3 +16,28 @@ con.connect(function(err) {
     console.log(result);
   });
 });
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url,true);
+  if(q.type === 'login'){
+	var sql = 'SELECT * FROM users WHERE user_name = ? AND user_pass = ?';
+	con.connect(function(err) {
+	  if (err) throw err;
+	  con.query(sql, [q.user, q.pass], function (err, result) {
+	    if (err) throw err;
+	    console.log(result);
+	  });	
+	});  
+  }
+  else if(q.type === 'register'){
+	var sql = 'INSERT INTO users (?) VALUES ?';
+	var val = ;
+	con.connect(function(err) {
+	  if (err) throw err;
+	  con.query(sql, [val], function (err, result) {
+	    if (err) throw err;
+	    console.log(result);
+	  });	
+	});      
+  }	  
+}).listen(8080);
