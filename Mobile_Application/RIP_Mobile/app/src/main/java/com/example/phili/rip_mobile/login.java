9b4f472b2cd58dc2314bbdd49bcec0723eff6cee
Philip <24674672@student.g.nwu.ac.za>
2017-09-02 17:04:18 +0200

    Can no login with app

--- Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/login.java
+++ Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/login.java
@@ -14,12 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class login extends AppCompatActivity implements View.OnClickListener{
@@ -30,7 +34,7 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     Context contxt;
     private RequestQueue reqQ;
     private StringRequest stringReq;
-    private String url = "http://52.15.226.85:8080/test";
+    private String url = "http://52.15.226.85:8080";
     private static final String TAG = login.class.getName();
 
     @Override
@@ -54,7 +58,7 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View v){
         if(v.getId() == R.id.btnSend) {
             try {
-                sendGet();
+                sendPost();
             }
             catch (Exception e){
 
@@ -80,4 +84,39 @@ public class login extends AppCompatActivity implements View.OnClickListener{
         reqQ.add(stringReq);
 
     }
+
+    private void sendPost(){
+        reqQ = Volley.newRequestQueue(this);
+        stringReq = new StringRequest(Request.Method.POST, url + "/api/login", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG,"Response: " + response.toString());
+                tvReturn.setText(results(response.toString()));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG,"Error: " + error.toString());
+            }
+        }){
+            @Override
+            public Map<String,String> getHeaders(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("username",etUsername.getText().toString());
+                params.put("password",etPass.getText().toString());
+                return params;
+        }};
+        reqQ.add(stringReq);
+    }
+
+    private String results(String in){
+        String temp = "";
+
+        String[] result = in.split(",");
+
+        temp = result[0];
+
+        return temp;
+    }
+
 }
