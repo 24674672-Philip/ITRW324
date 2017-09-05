d71a1aebf5f04c5f77a3a700b7da066a32cb60ca
Theunnis <25911864@student.g.nwu.ac.za>
2017-09-05 17:41:17 +0200

    New app screens added (not final) + Navigation
    - Registration screen added
    - Splash screen added
    - Login screen updated 
    - Main menu screen added

--- Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/Register.java
+++
@@ -1,8 +0,0 @@
-package com.example.phili.rip_mobile;
-
-/**
- * Created by tcjvr on 2017/09/04.
- */
-
-public class Register {
-}
---
+++ Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/registration.java
@@ -0,0 +1,43 @@
+package com.example.phili.rip_mobile;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class registration extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+    }
+}
