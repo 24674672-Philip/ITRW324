f45485d99fcf6337cacf7e07d9436522cbb4dbcb
Vorster Naude <26192349@student.nwu.ac.za>
2017-10-04 11:49:49 +0200

    music-list fixed

--- Website/RIPWebsite/src/app/services/music-player.service.ts
+++ Website/RIPWebsite/src/app/services/music-player.service.ts
@@ -7,7 +7,7 @@ export class MusicPlayerService {
   currentPlaylist = new Array<Song>();
   //currentSong: Song = new Song('Default', 'Default', 'Default','../../favicon.ico','Default');
   //TODO: replace with server data
-  currentSong: Song = new Song(1,1,1,'Jack Parow', 'Cooler as ekke', 'Parow to Paarl', 'http://images.genius.com/165ba96222b44eaf259801336390a18c.640x640x1.jpg','http://52.211.85.57:8080/api/music?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoidGVzdCJ9LCJpYXQiOjE1MDcwMjIwNzB9.9SuwgDCN6xSn44-ZqLr_mpO8S86aBHeuSoY6jNkSqXo&song=Nightmare&album=Nightmare&artist=Avenged%20Sevenfold');
+  currentSong: Song = new Song(1,1,1,'Jack Parow', 'Cooler as ekke', 'Parow to Paarl', 'http://images.genius.com/165ba96222b44eaf259801336390a18c.640x640x1.jpg') ;
   constructor() {
 
   }
