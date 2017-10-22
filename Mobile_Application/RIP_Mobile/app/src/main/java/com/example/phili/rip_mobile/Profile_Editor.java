3aa1fcce3a4d9f101ed990ac298ad0a4599eea42
Theunnis <25911864@student.g.nwu.ac.za>
2017-10-22 23:08:29 +0200

    Display Bio (In progress)

--- Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/Profile_Editor.java
+++ Mobile_Application/RIP_Mobile/app/src/main/java/com/example/phili/rip_mobile/Profile_Editor.java
@@ -1,11 +1,19 @@
 package com.example.phili.rip_mobile;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -13,6 +21,8 @@ import android.view.ViewGroup;
  */
 public class Profile_Editor extends Fragment {
 
+
+    EditText etBio;
 
     public Profile_Editor() {
         // Required empty public constructor
@@ -23,7 +33,63 @@ public class Profile_Editor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile__editor, container, false);
+        View v =  inflater.inflate(R.layout.fragment_profile__editor, container, false);
+
+       // etBio = (EditText) getView().findViewById(R.id.etBio);
+
+       getBio();
+        return v;
     }
 
+
+
+    private void getBio() {
+
+        if (isOnline() == true) {
+            String[] headersType = new String[1];
+            String[] headersVal = new String[1];
+            headersVal[0] = login.USERNAME;
+            headersType[0] = "Artist";
+
+            final serverLink sender = new serverLink(getActivity());
+            sender.sendServerRequest(headersType, headersVal, "/api/getartists", true, new serverLink.OnDownloadTaskCompleted() {
+                @Override
+                public void onTaskCompleted(JSONObject result, boolean error, String message) {
+                    try {
+                        if (result.getString("result").contains("success")) {
+                            Toast.makeText(getActivity(),result.getString("bio"), Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        Toast.makeText(getActivity(),e.getMessage().toString() , Toast.LENGTH_LONG).show();
+                        // tvReturn.setText(e.getMessage().toString());
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(),e.getMessage().toString() , Toast.LENGTH_LONG).show();
+                        // tvReturn.setText(e.getMessage().toString());
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getActivity(), "You are not connected to Internet", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected boolean isOnline() {
+
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
 }
